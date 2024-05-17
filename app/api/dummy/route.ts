98e6f737461fd998ef61632a357f6600a159fbe8
dayo <ifedayoadeniyi@gmail.com>
2024-05-17 10:08:58 +0000

    fix

--- app/api/dummy/route.ts
+++ app/api/dummy/route.ts
@@ -3,6 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (request: NextRequest) => {
     const body = await request.json()
+    const whooks = await kv.get("webhooks")
+    if (!whooks) {
+        await kv.set("webhooks", [])
+    }
     await kv.json.arrappend("webhooks", body)
     return new NextResponse("success", {status: 200})
 }
