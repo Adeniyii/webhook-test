f57dd0b1975bcc1497f71f4f525a926f9f683b1e
dayo <ifedayoadeniyi@gmail.com>
2024-05-17 10:42:59 +0000

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
 import { kv } from "@vercel/kv";
 
+export const dynamic = "force-dynamic"
+
 export default async function Home() {
   const webhooks = await kv.get<any[]>("webhooks");
-  
+
   console.log({webhooks})
   
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <p>List of received webhook triggers</p>
-    <div>
-      {webhooks?.map((wh) => (
+    <div className="flex flex-col gap-6">
+      {webhooks?.map((wh, idx) => (
         <div key={wh.id}>
+          <p>{idx}</p>
           <pre>
-            {JSON.stringify(wh)}
+            {JSON.stringify(wh, undefined, 4)}
           </pre>
+          <p className="mt-4">------------------------------------------------------------------</p>
         </div>
       ))}
     </div>
