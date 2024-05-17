2ca2616a7de2e968cdb5d4cd7c682bc3b22c1eb1
dayo <ifedayoadeniyi@gmail.com>
2024-05-17 10:56:49 +0000

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export const dynamic = "force-dynamic"
 export default async function Home() {
   const webhooks = await kv.get<any[]>("webhooks");
 
-  console.log({webhooks})
+  console.log(JSON.stringify(webhooks))
   
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
